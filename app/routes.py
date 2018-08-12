be0951c93d4ab100481f82f68e4777ecaee5c9bc
Charlene Fama <charlenefama18@gmail.com>
2018-08-13 04:18:19 +0800

    fix configuration files

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
+from Controllers.Admin_Controller import *
 
 from app import models
 
@@ -23,12 +24,15 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        elif user.role == 'admin':
-            return redirect(url_for('dashboard'))
+
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
+
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            if user.role == "admin":
+                next_page = url_for('dashboard')
+            else:
+                next_page = url_for('index')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
@@ -62,14 +66,14 @@ def register():
 @login_required
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home', user=user, page='Dashboard')
 
 
 # Admin Index
-@app.route('/dashboard')
+@app.route('/admin-dashboard')
 @login_required
 def dashboard():
-    return
+    return admin_dashboard()
 
 
 # Error Handling
