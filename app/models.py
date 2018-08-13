77c1d3b44a9bf406d5e5e002b98ae92213fece47
Charlene Fama <charlenefama18@gmail.com>
2018-08-13 17:48:45 +0800

    return token in login

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
+import os
+
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -23,4 +29,20 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(os.environ.get('SECRET_KEY'), expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(os.environ.get('SECRET_KEY'))
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+        user = User.query.get(data['id'])
+        return user
 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, request
+import json
+from flask import render_template, flash, redirect, url_for, request, Response, jsonify, g
 from app import app
 from app import db
 from app.forms import *
@@ -6,34 +7,56 @@ from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
-from Controllers.Admin_Controller import *
-from werkzeug.urls import url_parse
-from app import models
+from Controllers import Admin_Controller as ac
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 
 
 # Log In User
 @app.route('/admin', methods=['GET', 'POST'])
 @app.route('/users/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({ 'token': token.decode('ascii') })
 
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
 
-        if not next_page or url_parse(next_page).netloc != '':
-            if user.role == "admin":
-                next_page = url_for('dashboard')
-            else:
-                next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.check_password(password):
+            return False
+    g.user = user
+    return True
+
+# @app.route('/admin', methods=['GET', 'POST'])
+# @app.route('/users/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+#
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#
+#         if not next_page or url_parse(next_page).netloc != '':
+#             if user.role == "admin":
+#                 next_page = url_for('dashboard')
+#             else:
+#                 next_page = url_for('index')
+#         return redirect(next_page)
+#         # return redirect(next_page)
+#     return render_template('login.html', title='Sign In', form=form)
 
 
 # User Logout
@@ -72,13 +95,14 @@ def index():
 @app.route('/admin-dashboard')
 @login_required
 def dashboard():
-    return admin_dashboard()
+    return render_template('admin/dashboard.html', title='Dashboard', page='Dashboard')
 
 # Admin Index
-@app.route('/users-list')
+@app.route("/users-list")
 @login_required
-def dashboard():
-    return admin_dashboard()
+def users_list():
+    return ac.get_users()
+    # return render_template("admin/users.html", title='Users', page='Users List', data=ac.get_users())
 
 
 # Error Handling
@@ -90,4 +114,4 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('error/500.html'), 500
