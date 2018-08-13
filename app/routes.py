0c89c10344a624b82363e237d7f3abb43a491830
Charlene Fama <charlenefama18@gmail.com>
2018-08-14 00:16:49 +0800

    fixed error handling

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ from flask_httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
 
+from app import models
 
 # Log In User
 @app.route('/admin', methods=['GET', 'POST'])
@@ -130,16 +131,16 @@ def book():
 # Error Handling
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('error/404.html'), 404
+    return render_template('error/empty.html', message="NOT FOUND"), 404
 
 
 # Error Handling
 @app.errorhandler(401)
 def authentication_error(error):
-    return "Not Authorized"
+    return render_template('error/empty.html', message="NOT AUTHORIZED"),
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('error/500.html'), 500
+    return render_template('error/empty.html', message="INTERNAL ERROR"), 500
