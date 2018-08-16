fedac0d726ae15c19d428f1ee198541e6276a0d9
Charlene Fama <charlenefama18@gmail.com>
2018-08-16 12:03:03 +0800

    fixed return statement of error handling

--- Controllers/Login_Controller.py
+++ Controllers/Login_Controller.py
@@ -11,6 +11,9 @@ auth = HTTPBasicAuth()
 @app.route('/users/login', methods=['GET', 'POST'])
 @auth.login_required
 def get_auth_token():
+    if verify_password is False:
+        return render_template('error/empty.html', message="Invalid username/password supplied"), 404
+
     token = g.user.generate_auth_token()
     return jsonify({'token': token.decode('ascii')})
 
@@ -19,13 +22,12 @@ def get_auth_token():
 def verify_password(username_or_token, password):
     # first try to authenticate by token
     user = User.verify_auth_token(username_or_token)
-    if not user:
+    if not user or user is None:
         # try to authenticate with username/password
         user = User.query.filter_by(username=username_or_token).first()
         if not user or not user.check_password(password):
-            # return False
-            return render_template('error/empty.html', message="Invalid username/password supplied"), 404
-
+            return False
+            # return render_template('error/empty.html', message="Invalid username/password supplied"), 404
     g.user = user
     return True
 
