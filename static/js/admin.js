682d786ddb7d6302f240839d0254330480ece32c
DeanEarl <deanfailed@gmail.com>
2018-08-20 23:25:54 +0800

    fixed css for listing of books

--- static/js/admin.js
+++ static/js/admin.js
@@ -13,16 +13,12 @@ function initbooks() {
     }).done(function (data) {
         for (var i = 0; i <data.length; i++){
             var html = `<div class="column">
-              <div class="demo-title">Normal</div>
               <div class="post-module">
                 <div class="thumbnail">
-                  <div class="date">
-                    <div class="day">27</div>
-                    <div class="month">Mar</div>
-                  </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg"/>
                 </div>
                 <div class="post-content">
-                  <div class="category">Genre</div>
+                  <div class="category">\\Genre</div>
                   <h1 class="title">${data[i].book_name}</h1>
                   <h2 class="sub_title">${data[i].description}</h2>
                   <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
