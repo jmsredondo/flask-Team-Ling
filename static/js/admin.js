204c3948c3501b63a86d225c022b69303ff684fc
DeanEarl <deanfailed@gmail.com>
2018-08-21 16:35:03 +0800

    changed style of genres.html

--- static/js/admin.js
+++ static/js/admin.js
@@ -49,26 +49,14 @@ function initgenre() {
     }).done(function (data) {
         for (var i = 0; i < data.length; i++){
             var html = `<div class="fx-wrap"> 
-                <div class="card"
-             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZIbTuWRSqUVqo_Qdo4O9PKBwhRuwHBcmDdJlDez5oSwdXel-7pw">
+                <div class="card">
             <div class="front">
                 <div class="image">
-                    <p class="heading">${data[i].genre}</p>
+                    <p class="heading">${data[i].type}</p>
                 </div>
                 <div class="text">
-                    <header class="clearfix">
-                        <button class="flip"><span class="zmdi zmdi-replay"></span></button>
-                    </header>
-                    <p>Description</p>
+                    <p>${data[i].genre}</p>
                 </div>
-            </div>
-            <div class="back">
-              <div class="text">
-                <header class="clearfix">
-                  <button class="flip"><span class="zmdi zmdi-replay"></span></button>
-                </header><b>"You can flip me all day."</b>
-                <p>I have flexbox with fallbacks, I'm not browser prefixed (so watch out, this is a prototype) but my animations are pure css if your into that sort of thing.</p>
-              </div>
             </div>
           </div>
         </div> 
