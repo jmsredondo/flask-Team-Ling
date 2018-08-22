675e50fa52ee21a18fc406dde8524d5ab9e49998
DeanEarl <deanfailed@gmail.com>
2018-08-22 21:36:01 +0800

    added css for shortening long text in book views

--- static/js/admin.js
+++ static/js/admin.js
@@ -19,7 +19,7 @@ function initbooks() {
                 </div>
                 <div class="post-content">
                   <h1 class="title">${data[i].book_name}</h1>
-                  <h2 class="sub_title">${data[i].description}</h2>
+                  <h2 class="sub_title shorten">${data[i].description}</h2>
                   <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
                   <div class="post-meta">
                  </div>
