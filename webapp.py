39b04511e51666591673d82e81685daf08a2dec4
Danzel Taccayan (IS-PH) <Danzel_Taccayan@trendmicro.com>
2018-08-28 09:00:20 +0800

    changed ports

--- webapp.py
+++ webapp.py
@@ -358,5 +358,5 @@ def logout():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9500)
-    # app.run(debug=True, port=80)
+    # app.run(host='0.0.0.0', port=9500)
+    app.run(debug=True, port=80)
